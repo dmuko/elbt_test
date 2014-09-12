bca6986847b99767a5bf160f28835be2bdd6bc46
Teemu Korpela <teemu@motleyagency.com>
2014-09-12 08:17:30 +0300

    Test update that requires pip install -r requirements.txt to be run.

--- application.py
+++ application.py
@@ -1,10 +1,15 @@
 from flask import Flask
+import bcrypt
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello brave new world!"
+    hashed = bcrypt.hashpw("saapas", bcrypt.gensalt())
+    print hashed 
+    return "Hello brave new world! %s" % hashed
 
 
 if __name__ == "__main__":
+    app.debug = True
     app.run(host='0.0.0.0')
